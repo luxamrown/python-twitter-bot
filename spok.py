38481548237f61680413233eb6744b047293e53b
luxamrown <luxamrownn@gmail.com>
2020-05-22 21:30:30 +0700

    first commit

---
+++ spok.py
@@ -0,0 +1,5 @@
+#list yang berisi masing masing struktur kata dalam bahasa indonesia
+subjek = ["aku ", "saya ", "kamu ", "kalian ", "mereka ", "lu ", "gua "]
+predikat = ["sedang ", "lagi ", "mau ", "ingin ",]
+objek = ["mandi ", "makan ", "main ", "apa? ", "tidur "]
+keterangan = ["di rumah ", "di toilet ", "di sekolah "]
--- tweepybot.py
+++ tweepybot.py
@@ -1,23 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import tweepy, time, sys
-
-argfile = str(sys.argv[1])
-
-#Masukan informasi berisi token dan consumer_key anda disini :
-CONSUMER_KEY = '9dUYs7TrEI6mazA6T3YLBmF9E'
-CONSUMER_SECRET = 'FI5rxNOQ2eLHsQOF7bMoYq0MetGdgYN52tYZwLhH6vQGppmSmK'
-ACCESS_KEY = '975205761495179267-r8IrLn0ruPYhxBEF7xC9SxbwdcEkiat'
-ACCESS_SECRET = 'yjv3wBawT8dhSPrSnVKPFxGanDYe8U90PkGN974HxdqxZ'
+from random import randint
+from spok import *
+#token API di sini
+#There's API Key, from twitter.developer
+CONSUMER_KEY = 'CONSUMERKEYMILIKMU'
+CONSUMER_SECRET = 'CONSUMERKEYSECRETMILIKMU'
+ACCESS_KEY = 'ACCESSKEYMILIKMU' 
+ACCESS_SECRET = 'ACCESKEYSECRETMILIKMU'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-filename=open(argfile,'r')
-f=filename.readlines()
-filename.close()
-
-for line in f:
-    api.update_status(line)
-    time.sleep(20) #Tweet setiap 1 hari
+while True:
+    api.update_status(subjek[randint(0, len(subjek)-1)]+predikat[randint(0, len(predikat)-1)]+objek[randint(0, len(objek)-1)]+keterangan[randint(0, len(keterangan)-1)]+' -Bot')
+    time.sleep(86400)
+    #jeda antar twit dalam satuan detik
+    #pauses between twit in second
